ff554adae29df97cb9f3e8767e51385cb96adf3b
Joe Versoza <jversoza@users.noreply.github.com>
2017-03-23 15:09:33 -0400

    Create db.js

---
+++ db.js
@@ -0,0 +1,31 @@
+// users
+// * our site requires authentication...
+// * so users have a username and password
+// * they also can have 0 or more lists
+var User = new mongoose.Schema({
+  // username, password provided by plugin
+  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+});
+
+// an item (or group of the same items) in a grocery list
+// * includes the quantity of this item (multiple of the same item does not 
+//   require additional Item documents; just increase the quantity!)
+// * items in a list can be crossed off
+var Item = new mongoose.Schema({
+  name: {type: String, required: true},
+  quantity: {type: Number, min: 1, required: true},
+  checked: {type: Boolean, default: false, required: true}
+}, {
+  _id: true
+});
+
+// a grocery list
+// * each list must have a related user
+// * a list can have 0 or more items
+var List = new mongoose.Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  name: {type: String, required: true},
+  createdAt: {type: Date, required: true},
+  items: [Item]
+});
+```
